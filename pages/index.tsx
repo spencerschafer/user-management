e023898b352c601e8704b71d6aca4af9d809511c
spencerschafer <spencerschafer@gmail.com>
2021-12-23 23:59:04 +0200

    feat(project): add initial project structure

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type {NextPage} from 'next'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+    return (
+        <div className={styles.root}>
+            <header className={styles.header}>
+
+            </header>
+            <div className={styles.container}></div>
+        </div>
+    )
+}
+
+export default Home
